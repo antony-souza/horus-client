2509e414c04ebfaae997ffd49b0bd427473f3885
antony-souza <antonygustavo09@hotmail.com>
2024-08-06 22:21:50 -0300

    prod: Alterando positions

--- frontend/horus/src/app/pages/login/pages.tsx
+++ frontend/horus/src/app/pages/login/pages.tsx
@@ -4,82 +4,83 @@ export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        const body =  {
-            email:email,
-            password:password
-        }
+        const body = {
+            email: email,
+            password: password,
+        };
 
         try {
-            
-        const response = await fetch('http://localhost:3002/auth',{
+            const response = await fetch('http://localhost:3002/auth', {
+                method: 'POST',
+                mode: 'cors',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(body),
+            });
 
-            method:"POST",
-            mode:"cors",
-            headers: {"Content-Type":"application/json"},
-            body: JSON.stringify(body)
+            if (!response.ok) {
+                throw new Error('Falha na auth!');
+            }
 
-        });
+            const data = await response.json();
 
-        if(!response.ok){
-            throw new Error('Falha na auth!')
+            localStorage.setItem('token', data.token);
+            console.log('Login feito!', data);
+        } catch (error) {
+            console.error('Error ao fazer auth:', error);
         }
-
-        const data = await response.json()
-
-        localStorage.setItem('token', data.token)
-        console.log('Login feito!', data)
-
-    } catch (error) {
-        console.error("Error ao fazer auth:",error)
-    }
-
     };
 
     return (
-        <main className="min-h-screen flex items-center justify-center bg-gray-300 relative">
-            <form 
-                onSubmit={handleSubmit} 
-                className="bg-white p-8 rounded-lg shadow-md w-96 z-10"
-            >
-                <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-                <div className="mb-4">
-                    <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="email">
-                        Email
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
-                        placeholder="Digite seu email"
-                    />
-                </div>
-                <div className="mb-6">
-                    <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="password">
-                        Senha
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
-                        placeholder="Digite sua senha"
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="w-full bg-cyan-600 text-white font-semibold py-2 rounded-md hover:bg-cyan-700 transition duration-200"
+        <main className="min-h-screen flex items-start justify-center bg-gray-300 relative">
+            <div className="flex-grow flex items-center justify-center">
+                <form 
+                    onSubmit={handleSubmit} 
+                    className="bg-white p-8 rounded-lg shadow-md w-96 z-10 mt-56"
                 >
-                    Entrar
-                </button>
-            </form>
+                    <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
+                            placeholder="Digite seu email"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="password">
+                            Senha
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
+                            placeholder="Digite sua senha"
+                        />
+                    </div>
+                    <div className='flex flex-col gap-3'>
+                    <button
+                        type="submit"
+                        className="cursor-pointer w-full bg-cyan-600 text-white font-semibold py-2 rounded-md hover:bg-cyan-700 transition duration-200"
+                    >
+                        Entrar
+                    </button>
+                    <button className='text-gray-400 cursor-pointer'>Esqueci email ou senha</button>
+                    </div>
+                   
+                </form>
+            </div>
         </main>
     );
 }
