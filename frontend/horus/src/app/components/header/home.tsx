7951749c56032817239ad79300699ecfa345fd2f
antony-souza <antonygustavo09@hotmail.com>
2024-08-06 22:41:37 -0300

    Teste

--- frontend/horus/src/app/components/header/home.tsx
+++ frontend/horus/src/app/components/header/home.tsx
@@ -2,7 +2,7 @@ export default function Header(){
     return<>
         <header className="bg-cyan-600 w-100">
             <nav className="flex items-center justify-center">
-                <img src="/horus.png" className="w-20 pl-2"/>    
+                <img src="/horus.png" className="w-20 h-25"/>    
             </nav>
         </header>
     </> 
--- frontend/horus/src/app/pages/login/pages.tsx
+++ frontend/horus/src/app/pages/login/pages.tsx
@@ -34,42 +34,41 @@ export default function Login() {
     };
 
     return (
-        <main className="min-h-screen flex items-start justify-center bg-gray-300 relative">
-            <div className="flex-grow flex items-center justify-center">
-                <form 
-                    onSubmit={handleSubmit} 
-                    className="bg-white p-8 rounded-lg shadow-md w-96 z-10 mt-48"
-                >
-                    <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-                    <div className="mb-4">
-                        <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="email">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
-                            placeholder="Digite seu email"
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="password">
-                            Senha
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
-                            placeholder="Digite sua senha"
-                        />
-                    </div>
-                    <div className='flex flex-col gap-3'>
+        <main className="min-h-screen flex items-start justify-center bg-gray-300">
+            <form 
+                onSubmit={handleSubmit} 
+                className="bg-white p-8 rounded-lg shadow-md w-96 z-10 mt-44" // Ajuste a margem superior aqui
+            >
+                <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+                <div className="mb-4">
+                    <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
+                        placeholder="Digite seu email"
+                    />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="password">
+                        Senha
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
+                        placeholder="Digite sua senha"
+                    />
+                </div>
+                <div className='flex flex-col gap-3'>
                     <button
                         type="submit"
                         className="cursor-pointer w-full bg-cyan-600 text-white font-semibold py-2 rounded-md hover:bg-cyan-700 transition duration-200"
@@ -77,10 +76,8 @@ export default function Login() {
                         Entrar
                     </button>
                     <button className='text-gray-400 cursor-pointer'>Esqueci email ou senha</button>
-                    </div>
-                   
-                </form>
-            </div>
+                </div>
+            </form>
         </main>
     );
 }
