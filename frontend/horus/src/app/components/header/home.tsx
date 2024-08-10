4de57cc51da2caabd627863d1edb22755fbd716c
antony-souza <antonygustavo09@hotmail.com>
2024-08-09 23:58:07 -0300

    paginas de admin e manager + decoded token

---
+++ frontend/horus/src/app/components/header/home.tsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return<>
+        <header className="bg-cyan-600 w-100">
+            <nav className="flex items-center justify-center">
+                <img src="/horus.png" className="w-20 h-25"/>    
+            </nav>
+        </header>
+    </> 
+}
